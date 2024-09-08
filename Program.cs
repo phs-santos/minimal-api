5d5080bce2fb5ce8c4a34dc2c834e48ec84d4f17
Paulo Souza <paulo18santos2000@gmail.com>
2024-09-08 19:28:51 -0300

    build: melhorado a view da pagina inicial da api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.ModelViews;
 using MinimalApi.Dominio.Servicos;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
@@ -21,7 +22,7 @@ builder.Services.AddDbContext<DbContexto>(options => {
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdmnistradorServico admnistradorServico) => {
     if(admnistradorServico.Login(loginDTO) != null) {
