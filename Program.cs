fdfb35ce97fecbd9646f920cd4eff51ff89a8934
Paulo Souza <paulo18santos2000@gmail.com>
2024-09-08 19:58:15 -0300

    feat: implementado o metodo PUT para atualizar um veiculo

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DbContexto>(options => {
+builder.Services.AddDbContext<DbContexto>(options =>
+{
     options.UseMySql(
         builder.Configuration.GetConnectionString("mysql"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
@@ -30,41 +31,62 @@ app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Admnistrador
-app.MapPost("/admnistradores/login", ([FromBody] LoginDTO loginDTO, IAdmnistradorServico admnistradorServico) => {
-    if(admnistradorServico.Login(loginDTO) != null) {
+app.MapPost("/admnistradores/login", ([FromBody] LoginDTO loginDTO, IAdmnistradorServico admnistradorServico) =>
+{
+    if (admnistradorServico.Login(loginDTO) != null)
+    {
         return Results.Ok("Login com sucesso");
-    } else {
+    }
+    else
+    {
         return Results.Unauthorized();
     }
 }).WithTags("Administradores");
 #endregion
 
 #region Veiculos
-app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
-    Veiculo veiculo = new Veiculo {
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    Veiculo veiculo = new Veiculo
+    {
         Nome = veiculoDTO.Nome,
         Marca = veiculoDTO.Marca,
         Ano = veiculoDTO.Ano
     };
-    
+
     veiculoServico.Incluir(veiculo);
     return Results.Created($"/veiculos/{veiculo.Id}", veiculo);
 }).WithTags("Veiculos");
 
-app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) => {
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
+{
     List<Veiculo> veiculos = veiculoServico.Todos(pagina);
     return Results.Ok(veiculos);
 }).WithTags("Veiculos");
 
-app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => {
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
     Veiculo? veiculo = veiculoServico.BuscaPorId(id);
-    
-    if(veiculo == null) {
-        return Results.NotFound();
-    }
-    
+
+    if (veiculo == null) return Results.NotFound();
+
     return Results.Ok(veiculo);
 }).WithTags("Veiculos");
+
+app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    Veiculo? veiculo = veiculoServico.BuscaPorId(id);
+    if (veiculo == null) return Results.NotFound();
+
+    veiculo.Nome = veiculoDTO.Nome;
+    veiculo.Marca = veiculoDTO.Marca;
+    veiculo.Ano = veiculoDTO.Ano;
+
+    veiculoServico.Atualizar(veiculo);
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
 #endregion
 
 #region App
